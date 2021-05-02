a54eefa397a4b1b960fc3c9ad73995423a35ee97
toniminh161200 <52942703+toniminh161200@users.noreply.github.com>
2021-05-02 08:12:36 +0700

    Add files via upload

---
+++ LAB2/git/git4/E.java
@@ -0,0 +1,50 @@
+public class E extends K {
+
+    private int a = 1;
+
+    private int e = 1;
+
+    public byte oo() {
+        return 1;
+    }
+
+    public void ab() {
+        System.out.println();
+    }
+
+    public float ff() {
+        return 0;
+    }
+
+    public String kk() {
+        return "No";
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public double ad() {
+        return java.lang.Math.sqrt(13);
+    }
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+
+    public double ee() {
+        return 0.000001;
+    }
+
+    public int ae() {
+        return java.lang.Math.abs(-6);
+    }
+
+    public void aa() {
+        return;
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
+}
---
+++ LAB2/git/git4/K.java
@@ -0,0 +1,50 @@
+public class K extends null {
+
+    private double b = 100.500;
+
+    private byte g = 1;
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public byte oo() {
+        return 2;
+    }
+
+    public void bb() {
+        System.out.println(getClass().getName());
+    }
+
+    public long dd() {
+        return 33;
+    }
+
+    public String nn() {
+        "".>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.;
+    }
+
+    public int ae() {
+        return 9;
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.ArrayList<String>();
+    }
+
+    public int hh() {
+        return new java.util.Random().nextInt();
+    }
+
+    public void ab() {
+        return;
+    }
+
+    public String kk() {
+        return "Hello world";
+    }
+}
