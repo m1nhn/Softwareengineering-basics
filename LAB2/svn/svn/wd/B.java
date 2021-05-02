6c3c95036a85a15aa474e1e3e1a2cad849aa1564
toniminh161200 <52942703+toniminh161200@users.noreply.github.com>
2021-05-02 08:14:38 +0700

    Add files via upload

---
+++ LAB2/svn/svn/wd/B.java
@@ -0,0 +1,46 @@
+public class B extends null {
+
+    private byte d = 1;
+
+    private int g = 1;
+
+    public long ac() {
+        return 222;
+    }
+
+    public float ff() {
+        return 0;
+    }
+
+    public String kk() {
+        return "No";
+    }
+
+    public Object rr() {
+        return null;
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.HashSet<Integer>;
+    }
+
+    public void aa() {
+        return;
+    }
+
+    public double ee() {
+        return 500.100;
+    }
+
+    public int ae() {
+        return java.lang.Math.abs(-7);
+    }
+
+    public int cc() {
+        return 39;
+    }
+
+    public Object pp() {
+        return this;
+    }
+}
---
+++ LAB2/svn/svn/wd/F.java
@@ -0,0 +1,58 @@
+public class F extends null {
+
+    private long e = 4321;
+
+    private double d = 100.500;
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+
+    public String nn() {
+        return "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++";
+    }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public long ac() {
+        return 111;
+    }
+
+    public float ff() {
+        return 3.14;
+    }
+
+    public void aa() {
+        System.out.println("Hello world!");
+    }
+
+    public String kk() {
+        return "Yes";
+    }
+
+    public Object rr() {
+        return null;
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.HashSet<Integer>;
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public int[] ii() {
+        return new int[]{4, 3, 2, 1};
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.ArrayList<String>();
+    }
+
+    public Object pp() {
+        return this;
+    }
+}
