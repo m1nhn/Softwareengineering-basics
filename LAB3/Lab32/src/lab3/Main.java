394ac1cca74595b4686f4b8b6679d4885ed48658
toniminh161200 <52942703+toniminh161200@users.noreply.github.com>
2021-05-22 16:26:36 +0700

    Delete LAB3/Lab32 directory

--- LAB3/Lab32/src/lab3/Main.java
+++
@@ -1,9 +0,0 @@
-package lab3;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    MyMath math = new MyMath();
-        System.out.println("Result is : " + math.multi(10,11));
-    }
-}
--- LAB3/Lab32/src/lab3/MyMath.java
+++
@@ -1,8 +0,0 @@
-package lab3;
-
-public class MyMath {
-    private final int e = 1;
-    public int multi(int a, int b){
-        return a*b;
-    }
-}
