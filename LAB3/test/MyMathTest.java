d773b09d90163cabd23046a2da55f45833955e52
toniminh161200 <52942703+toniminh161200@users.noreply.github.com>
2021-05-25 00:27:38 +0700

    Update some tests for lab 3

--- LAB3/test/MyMathTest.java
+++ LAB3/test/MyMathTest.java
@@ -15,4 +15,18 @@ public class MyMathTest {
         MyMath test2 = new MyMath();
         assertEquals(970,test2.multi(97,10));
     }
+
+    @Test
+    public void testAssertions(){
+        int val1 = 5;
+        int val2 = 6;
+        assertTrue(val1 < val2);
+    }
+
+    @Test
+    public void fourthTest(){
+        String [] expectedArray = {"one", "two", "three"};
+        String [] resultArray = {"one", "two", "three"};
+        assertArrayEquals(expectedArray, resultArray);
+    }
 }
